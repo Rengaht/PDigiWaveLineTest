04b74bc0ac0324d9099b4da72b66948041c93ecb
reng <reng@ultracombos.com>
2021-10-14 11:08:40 +0800

    console & game @ Liff

--- index.js
+++ index.js
@@ -2,11 +2,117 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+const path = require('path');
+
+// line message webhook
+const line = require('@line/bot-sdk');
+const { isFunction } = require('util');
+const config = {
+    channelAccessToken: '3leojDtwVk0QMaU5fvkN4LrknnLLW8pe2gYRNX0Wf7MKFJ9vkr6Icw2dtpW+wOsChtbscMCI0OjpldBbKbGK4Ar1uu/ZBzIjSDwRyo9bCWhtTppbgRNa48b01urrKkPDxKLBPcL+aSFwZ8L8SCs1/gdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '7b4c45e3139178c9a9b2ec3dfc3a96cf',
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// session
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
+
+
+app.post('/callback', line.middleware(config), (req, res) => {
+    Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+    });
+    
+});
+
+// event handler
+function handleEvent(event,req) {
+
+    
+
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        // ignore non-text-message event
+        return Promise.resolve(null);
+    }
+    
+    var text=event.message.text;
+
+    if(text=="main"){
+        sendMainLiff(event.replyToken);
+    }
+
+    var id=event.source.userId;
+    
+   
+
+    if(!cache.has(id)){
+        cache.set(id,text);
+    }else{
+        cache.set(id, cache.get(id)+text);
+    }
+
+
+    // console.log(id+" -> "+cache.get(id));
+    // console.log(cache.keys());
+
+    if(cache.get(id).includes('↑↑↓↓←→←→BA')){
+        sendMainLiff(event.replyToken);
+        delete codeInput[id];
+    }
+    
+}
+function sendMainLiff(replyToken){
+    // create a echoing text message
+    const home_flex = {
+        "type": "flex",
+        "altText": "this is a flex message",
+        "contents": {
+          "type": "bubble",
+          "body": {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+            {
+                "type": "button",
+                "style": "primary",
+                "action": {
+                "type": "uri",
+                "label": "主展場",
+                "uri": "https://liff.line.me/1656511168-r2WY13kl"
+                }
+            }
+            ]
+          }
+        }
+      }
+
+
+
+    // use reply API
+    return client.replyMessage(replyToken, home_flex);
+}
+
 
 app.use(express.static('public'));
 
 app.get('/send-id', function(req, res) {
     res.json({id: myLiffId});
+});
+
+// app.get('/code', function(req, res){
+//     console.log('get code');
+//     res.send('www');
+// });
+
+
+app.get('/game', function(req, res){
+    res.sendFile(path.join(__dirname, '/public/game.html'));
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
